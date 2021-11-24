c93c6fdb2b802b34d6e19ef2b7c310189d4ca5a8
Aufalia <91171324+aufalia@users.noreply.github.com>
2021-11-25 00:50:58 +0800

    Add files via upload

---
+++ round_button_icon.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  const RoundIconButton({@required this.icon, @required this.onPressed});
+
+  final IconData icon;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      onPressed: () {
+        onPressed();
+      },
+    );
+  }
+}
